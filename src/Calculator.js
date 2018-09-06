ad52e2c8e37fc13bf58f83852acdd9077a2a95c0
Stephanie <stephcshea@gmail.com>
2018-09-06 19:06:11 -0400

    updating file setup

---
+++ src/Calculator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="Calculator">
+          <div className="calculatorKey">C</div>
+          <calculatorKey>1</calculatorKey>
+          <calculatorKey>2</calculatorKey>
+          <calculatorKey>3</calculatorKey>
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
